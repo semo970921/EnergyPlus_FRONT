ca06d210eb5dffd312ad20eff9963fa8cfd08650
SeungWon <semo970921@naver.com>
2025-05-06 12:51:06 +0900

    feat/비밀번호찾기: 이메일 인증

---
+++ react/src/components/Password/PasswordRecovery.jsx
@@ -0,0 +1,14 @@
+
+const PasswordRecovery = () => {
+    return(
+        <Container>
+            <RecoveryForm>
+                <Title>비밀번호 찾기</Title>
+
+            </RecoveryForm>
+        </Container>
+    );
+}
+
+
+export default PasswordRecovery;
