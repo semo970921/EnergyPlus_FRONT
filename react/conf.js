fd1b360eff5152f3b9d00b40c1528f2c3dd62f93
Seongmin <133196887+2Seongmin@users.noreply.github.com>
2025-05-22 14:20:35 +0900

    feat/conf.js 추가

---
+++ react/conf.js
@@ -0,0 +1,5 @@
+window.ENV = {
+  API_URL: "http://localhost:8001",
+  KAKAO_CLIENT_ID: "1f79917de9031445745e34942341391c",
+  VITE_KAKAO_REDIRECT_URI: "http://localhost:80/oauth2/kakao/callback",
+};
